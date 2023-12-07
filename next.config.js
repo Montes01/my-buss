0b334b27832f4f0d5872f7c2c82a1413969fd065
sebastiangallego0317 <samuelmontes1105@gmail.com>
2023-12-07 13:41:21 -0500

    Add webpack configuration for CSS modules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.module\.css$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+          },
+        },
+      ],
+    });
+  },
+};
 module.exports = nextConfig;
